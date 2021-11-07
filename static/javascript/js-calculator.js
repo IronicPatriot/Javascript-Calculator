7fb48b586c2b8f1e137d727b082dadfd2b0c6bc5
IronicPatriot <65298158+IronicPatriot@users.noreply.github.com>
2021-11-07 12:33:07 +0000

    Add files via upload

---
+++ static/javascript/js-calculator.js
@@ -0,0 +1,103 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+    }
+
+    clear(){
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+                                        //takes off last character in the string
+    }
+    appendNumber(number){
+        if (number === '.' && this.currentOperand.includes('.')) return
+        // if user puts decimal and currentOperand already has one it aborts
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+                         // converted to string so we don't add together, we are only typing here (5 and 5 is 55 not 10)
+    }
+    chooseOperation(operation){
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+        // sets current number to previous when we select an operator and clears current
+    }
+    compute(){
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        // returns numbers to integers
+        if (isNaN(prev) || isNaN(current)) return
+        // if there is no prev or current value then return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case 'x':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        } //javascript if statement
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand
+        this.previousOperandTextElement.innerText = this.previousOperand
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const clearButton = document.querySelector('[data-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const equalsButton = document.querySelector('[data-equals]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+clearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
